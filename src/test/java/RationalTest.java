437fcb3fa57e12128545fe4901fe0db381ac12c5
MingRuji6388012 <rujiphart.cha@student.mahidol.edu>
2023-09-12 12:02:05 +0700

    Implemented the Subtract, Multiply, and Divide method

--- src/test/java/RationalTest.java
+++ src/test/java/RationalTest.java
@@ -1,9 +1,8 @@
 import org.junit.Assert;
 import org.junit.Test;
-
 public class RationalTest {
     @Test
-    public void testAdd() {
+    public void testAdd() throws Rational.Illegal {
         Rational x = new Rational();
         x.numerator = 1;
         x.denominator = 2;
@@ -11,6 +10,7 @@ public class RationalTest {
         y.numerator = 1;
         y.denominator = 4;
         x.add(y);
+        Rational d = new Rational(1,3);
         Assert.assertEquals(3, x.numerator);
     }
 }
